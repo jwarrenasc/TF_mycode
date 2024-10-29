f71b2d69d9071c07bcd649e3aa0ab4c475fd7051
jwarrenasc <jeremy.warren@ascension.org>
2024-10-29 13:10:51 +0000

    docker challenge

---
+++ docker-challenge/variables.tf
@@ -0,0 +1,22 @@
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type    = string
+  default = "Alta3ResearchWebService"
+  }
+  
+  
+variable "internal_port" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type    = number
+  default = 9876
+  }
+  
+variable "external_port" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type    = number
+  default = 5432
+  }
+
